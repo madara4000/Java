772684dcc181bf2767e74bf1ce5b2d9c62050b41
Yago Martins <yagomartins4000@gmail.com>
2023-03-15 17:12:41 -0300

    gama

---
+++ javateste/java-anatomia-de-classes/src/MinhaClasse.java
@@ -0,0 +1,15 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+       System.out.print("hello world");
+      String br="brasil";
+      br="brazil";
+      final double PI=3.14;
+      boolean verdadeira=true;
+     int numero1=4;
+     int numero2=8;
+     int soma= somar(numero1,numero2);
+     System.out.println(soma);
+    }public static  int somar (int numero1 , int numero2){
+ return numero1+numero2;
+    }
+}
