4e04679c6f8cf50ea05cf3625618081bf8f038a7
madara4000 <yagomartins4000@gmail.com>
2023-06-12 21:10:36 -0300

    novo commit java

---
+++ Exercicio1/minha classe/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("como você está");
+        String BR="brasil";
+        final String Uru="Uruguai";
+       int numero=2022;
+       String primeironome="Yago Martins Pintos";
+
+    numero=2023;   
+public static  int  somar( int numero1,int numero2){
+    return "numero1"+"numero2";
+}}
+}
---
+++ Exercicio1/minha classe/src/boletim/BoletimEstudantil.java
@@ -0,0 +1,16 @@
+package boletim;
+
+import javax.lang.model.util.ElementScanner14;
+
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal=5;
+        if(mediaFinal<7)
+        System.out.println("REPROVADO!");
+       else if(mediaFinal==
+       7)
+       System.out.println("Passado");
+      else
+      System.out.println("Otimo continua assim");
+    }
+}
